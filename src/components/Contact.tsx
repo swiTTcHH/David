1e6aa062745c1405e100108950352d9fc3f23082
swiTTcHH <dvdokpeta@gmail.com>
2026-07-09 23:45:07 +0100

    remove all unused React imports

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 export default function Contact() {
   const [formData, setFormData] = useState({ name: "", email: "", message: "" });
